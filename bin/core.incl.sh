f6c170ccd07676f30e595cb3a5327fb7ae17f82c
Socheat Sok <alex@socheat.net>
2020-03-12 15:28:20 +0700

    install theme from Gluu chroot env instead of main server

--- bin/core.incl.sh
+++ bin/core.incl.sh
@@ -1,8 +1,7 @@
-OPT_GLUU_SERVER=/opt/gluu-server
-OPT_DEPLOY_TARGET=${OPT_GLUU_SERVER}/opt/gluu/jetty
+OPT_DEPLOY_TARGET=/opt/gluu/jetty
 
 function war_template_dir() {
     local service=$1
 
-    echo ${OPT_GLUU_SERVER}/opt/jetty-*/temp/jetty-localhost-*-${service}.war*.dir/webapp
+    echo /opt/jetty-*/temp/jetty-localhost-*-${service}.war*.dir/webapp
 }
